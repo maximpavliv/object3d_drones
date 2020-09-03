9cb4280297b7a442c90d8c898b3f6402483d8858
Yang Zhou <yangzhou@nyu.edu>
2020-09-03 06:24:28 -0400

    modified some stuff on lambda quad

--- code/vis_wp_gt.m
+++ code/vis_wp_gt.m
@@ -1,12 +1,17 @@
 function vis_wp_gt(img, opt, heatmap, center, scale, cad, dict, rot_gt)
 
+    %estimated
     img_crop = cropImage(img,center,scale);
     S = bsxfun(@plus,opt.R*opt.S,[opt.T;0]);
     model = fullShape(S,cad,dict.kpt_id);
     mesh2d = model.vertices(:,1:2)'*200/size(heatmap,2);
+    %gt
+    S_gt = bsxfun(@plus,rot_gt*opt.S,[opt.T;0]);
+    model_gt = fullShape(S_gt,cad,dict.kpt_id);
+    mesh2d_gt = model_gt.vertices(:,1:2)'*200/size(heatmap,2);
     
     % visualization
-    nplot = 4;
+    nplot = 5;
     h = figure('position',[100,100,nplot*300,300]);
     % cropped image
     subplot('position',[0 0 1/nplot 1]);
@@ -29,5 +34,9 @@ function vis_wp_gt(img, opt, heatmap, center, scale, cad, dict, rot_gt)
     view(0,-90);
     set(h2,'XTick',[],'YTick',[]);
     set(h2,'visible','off')
+    %gt projected on image
+    %subplot('position',[4/nplot 0 1/nplot 1]);
+    %imshow(img_crop); hold on;
+    %patch('vertices',mesh2d_gt','faces',model_gt.faces,'FaceColor','red','FaceAlpha',0.3,'EdgeColor','none');
 
 end
